cb4cb06a90268d82e8d173c668f0babad38b5118
Ana Julia <anajuliadefendi@gmail.com>
2024-08-21 11:00:12 -0300

    projeto em branco

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+export default function App(){
+
+  return(
+    <>
+      <h1>Título</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo quae fugit, vitae unde sapiente provident, tenetur dolorum, impedit nihil accusantium sed nulla modi ullam cum sint deleniti odio veritatis? Earum!</p>
+    </>
+  )
+}
