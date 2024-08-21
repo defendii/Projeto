08548a1050aa074740b66691f24b4ed83d371122
Ana Julia <anajuliadefendi@gmail.com>
2024-08-21 11:47:01 -0300

    componente - snippets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+
 
 export default function App(){
 
   return(
     <>
-      <h1>Título</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo quae fugit, vitae unde sapiente provident, tenetur dolorum, impedit nihil accusantium sed nulla modi ullam cum sint deleniti odio veritatis? Earum!</p>
+     <Header/>
+     <Main/>
+     <Footer/>
     </>
   )
 }
