d5e598ce0b619ececb61421bf363498095a89cb2
Ana Julia <anajuliadefendi@gmail.com>
2024-08-30 09:37:27 -0300

    tasklist
    copiar i resto no moodle

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import Header from "./components/Header/Header"
 import Main from "./components/Main/Main"
 import Footer from "./components/Footer/Footer"
 
+import styles from "./App.module.css"
 
 export default function App(){
 
   return(
-    <>
+    <div className={styles.App}>
      <Header/>
      <Main>
-      
+        <form action="">
+          <input type="text" />
+          <button>+</button>
+        </form>
+        {/* <TaskList/> */}
      </Main>
      <Footer autor="Defendi"/>
-    </>
+    </div>
   )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,8 +3,21 @@ import styles from './Main.module.css'
 export default function Main (props){
     let {chlidren} = props
     return(
+        // <div className={styles.Main}>
+        //     {chlidren}
+        // </div>
         <div className={styles.Main}>
-            {chlidren}
+            <div className={styles.adicionar}>
+                <input type="text" />
+                <button>+</button>
+            </div>
+            <div className={styles.itens}>
+                <div className={styles.item1}>
+                    <p>item 1</p>
+                    <button>X</button>
+                </div>
+            </div>
+            
         </div>
     )
 }
